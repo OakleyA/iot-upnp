9f98ef9173c60bd119b8ec28cc5e32179b66fd91
Will Newton <will.newton@gmail.com>
2019-02-12 15:53:23 +0000

    upnp-server.py: Add simple PoC server

---
+++ upnp-server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import upnp
+
+def start_server():
+    device = upnp.Device({
+        'deviceType': 'urn:schemas-upnp-org:device:MediaServer:1',
+        'st': 'urn:schemas-upnp-org:device:MediaServer:1',
+        'friendlyName': 'UPnP Test',
+        'uuid': '00a56575-78fa-40fe-b107-8f4b5043a2b0',
+        'manufacturer': 'Example',
+        'manufacturerURL': 'http://example.com',
+    })
+
+    for i in range(1, 20):        
+        service = upnp.Service({
+            'serviceType': 'urn:schemas-upnp-org:service:ContentDirectory:1',
+            'serviceId': 'example-com:serviceId:%d' % i,
+            'controlURL': 'a' * i
+        })
+        device.addService(service)
+
+    server = upnp.Announcer(device)
+    server.initLoop()
+    server.notify()
+    server.foreaver()
+    server.dispose()
+
+if __name__ == '__main__':
+    start_server()
